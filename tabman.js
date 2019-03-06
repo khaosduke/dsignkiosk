b00819840091ff498946aceeef4826d829a47905
khaosduke <khaosduke@gmail.com>
2019-03-06 18:38:18 -0500

    Implementing closing all tabs

--- tabman.js
+++ tabman.js
@@ -7,22 +7,50 @@ Add-on for FireFox by Wilfredo Crespo
 
 //Get all the current tabs
 
+//TODO: Setup, get all tabs, remove all but one
+//Close every but the active window
+//Setup listener for any new open Tabs
+//Close them as they open
+
+function setup() {
+  let allTabsQuery = browser.tabs.query({});
+
+  allTabsQuery.then( function(tabs){
+    let tabIds = tabs.map(tabObj=>tabObj.id);
+    console.log(tabIds);
+    //Remove the first tab id, we will leave that one alone for now;
+    tabIds.shift();
+    console.log(tabIds);
+  },
+  onError);
+}
+
 console.log("im here");
-var allTabsQuery = browser.tabs.query({});
+
 var allTabs;
 
-function onQuerySuccess(tabs) {
-  console.log(`Tabs: ${tabs.length}`);
-  var removing = tabs.remove(1);
-  removing.then(onRemoved, onError);
-}
+
 
 function onError(error) {
   console.log(`Error: ${error}`);
 }
 
-allTabsQuery.then(onQuerySuccess,onError);
+//allTabsQuery.then(onQuerySuccess,onError);
 
 function onRemoved() {
   console.log(`Removed`);
 }
+
+function onQuerySuccess(tabs) {
+  console.log(`Tabs: ${tabs.length}`);
+  console.log(tabs);
+  //var removing = browser.tabs.remove(14);
+  //removing.then(onRemoved, onError);
+}
+
+function removeMe(tab) {
+  console.log("go away");
+  browser.tabs.remove(tab.id);
+}
+setup();
+//browser.tabs.onCreated.addListener(removeMe)
